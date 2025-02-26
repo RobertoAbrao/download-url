11303ff3971066e24acff48d6015d46a0809c42d
Roberto Abrão <beto-a-p@hotmail.com>
2025-02-26 17:32:10 -0300

    Ajuste para rodar no Render.com

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def get_video(filename):
     return send_from_directory(DOWNLOAD_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
